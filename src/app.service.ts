a65b052a71b308d02474dd09bb2dd3812ad00be9
hqyuh <73837629+hqyuh@users.noreply.github.com>
2024-10-12 16:28:14 +0700

    chore: test github action

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,7 @@ export class AppService {
 	getHello(): string {
 		const name = packageJson.name;
 		const version = packageJson.version;
+		console.log(name, version);
 		return `${name} v${version}`;
 	}
 }
